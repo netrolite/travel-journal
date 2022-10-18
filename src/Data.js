9a060011a126ef21885fbcf050d8ae6b8e8d0d2b
matvey733 <morozovmatvej75@gmail.com>
2022-10-18 09:41:34 +0300

    Added styling

--- src/Data.js
+++ src/Data.js
@@ -26,4 +26,76 @@ export default [
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "place3.png"
     },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place1.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place2.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place3.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place1.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place2.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place3.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place1.png"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "place2.png"
+    }
 ]
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,30 @@
 import React from "react";
+import pinIcon from "../media/pin-icon.svg"
 
 export default function Card(props) {
     console.log(props.imageUrl);
 
     return (
         <div className="card">
-            <img alt="card-image" 
-            src={process.env.PUBLIC_URL + "/media/" + props.imageUrl} />
+            <img alt="card" 
+            src={process.env.PUBLIC_URL + "/media/" + props.imageUrl} 
+            className="card-image"/>
             
             <div className="info">
-                <span className="location">{props.location}</span>
+                <h2 className="location">
+                    <img src={pinIcon} alt="" width="7px" className="pin-icon" />
+                    {props.location.toUpperCase()}
+                </h2>
                 <span className="mapsUrl">
                     <a href={props.googleMapsUrl}>
                         View on Google Maps
                     </a>
                 </span>
-                <div className="title">{props.title}</div>
-                <span className="start-date">{props.startDate} - </span>
-                <span className="end-date">{props.endDate}</span>
-                <div className="desc">{props.description}</div>
+                <h3 className="title">{props.title}</h3>
+                <div className="start-end-date">
+                    {props.startDate} - {props.endDate}
+                </div>
+                <h4 className="desc">{props.description}</h4>
             </div>
         </div>
     )
